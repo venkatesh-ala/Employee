8a7f22103229336c64ad4b600d010366225cb794
Sankar.Ala <Sankar.Ala@Venkatesh>
2020-09-04 19:36:11 +0530

    1st file change

---
+++ src/company/Employee1.java
@@ -0,0 +1,41 @@
+package company;
+
+public class Employee1 {
+	int id;
+	String name;
+	int phnno;
+	int deptno;
+	double salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhnno() {
+		return phnno;
+	}
+	public void setPhnno(int phnno) {
+		this.phnno = phnno;
+	}
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+
+}
